130f4d605c87cdb7e7a132abe527cbcf76fdc960
DivyaUtla <divyachowdaryb0@gmail.com>
2024-06-02 00:15:42 -0400

    Initial commit

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(function(req, res, next) {
     next();
 });
 
-
 app.use(express.static(__dirname, "/public"));
 app.get('/',(req, res) => res.sendFile(path.join(__dirname,'/public','index.html')));
 
