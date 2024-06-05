a6ac498bf3b79f21f977390f6084e0e94caaccfd
DivyaUtla <divyautlapalli@gmail.com>
2024-06-04 21:38:31 -0400

    add mongoose and db url

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const mongoose=require("mongoose");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require('path');
+
+mongoose.connect(process.env.dbURL)
+    .then(console.log("DB Connected!"))
+    .catch(error => console.log(error));
 
 app.use(express.json());
 
