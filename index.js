96855aca30a744b54994369fa80aaf9cb3533a7c
DivyaUtla <divyautlapalli@gmail.com>
2024-06-20 15:28:12 -0400

    create react app and cleanup client folder

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use('/comment',commentRoutes);
 app.use(express.static(path.join(__dirname, "/public")));
 app.get('/',(req, res) => res.sendFile(path.join(__dirname,'/public','index.html')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
