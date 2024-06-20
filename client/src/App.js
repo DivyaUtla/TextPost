bd8a8350118ccbd682af49f86a8eb79f27e178d9
DivyaUtla <divyautlapalli@gmail.com>
2024-06-20 16:34:38 -0400

    create react app and cleanup client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Welcome to our App!
+      Welcome to my POST App!
     </div>
   );
 }
